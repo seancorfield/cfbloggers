38636e14dd8e3994e583d8a1fa1b8a23313f8178
Sean Corfield <sean@corfield.org>
2014-06-07 17:52:44 -0700

    Add docstrings since I'm making this code public :)

--- src/cfbloggers/core.clj
+++ src/cfbloggers/core.clj
@@ -1,4 +1,10 @@
 (ns cfbloggers.core
+  "Scratch code to fetch the OPML feeds list from coldfusionbloggers.org
+  and attempt to parse every feed listed (in parallel!) to find the most
+  recent blog post (title and date).
+  Produces an HTML page listing all of those blog posts, along with a
+  list of blogs that could not be parsed, and a list of blogs that could
+  not be reached at all (i.e., the RSS feed could not be retrieved)."
   (:require [clojure.data.json :as json]
             [clojure.string :as str]
             [clojure.xml :as xml]
@@ -9,21 +15,30 @@
 
 (def opml-url "http://www.coldfusionbloggers.org/opml.cfm")
 
-(def counter (atom 0))
+(def counter
+  "Just to show how many concurrent requests are active while we run."
+  (atom 0))
 
-(def io (agent nil))
+(def io
+  "Used to single-thread the console output to make it more readable,
+  so that output produced by multiple threads doesn't get interleaved."
+  (agent nil))
 
 (defn println-1
+  "A single-threaded println. Uses the io agent."
   [& args]
   (apply send io (fn [_ & args] (apply println args)) args)
   ;; must return nil - we're a substitute for println and some code assumes this!
   nil)
 
 (defn get-opml
+  "Return data structure representing parsed XML version of OPML feeds list."
   []
   (xml/parse opml-url))
 
 (defn string-as-input-stream
+  "Given a string, return an input stream of its bytes.
+  Note that we encode & to &amp; to avoid badly formatted blogs."
   [^String s]
   (-> s
       (str/replace #"&" "&amp;")
@@ -31,6 +46,11 @@
       (java.io.ByteArrayInputStream.)))
 
 (defn parse-rss
+  "Attempt to parse an RSS feeed. Returns nil if the URL doesn't
+  resolve or times out. Returns a string if we cannot parse the
+  RSS XML (because the request got a 404 Not Found, or the XML
+  is invalid for whatever reason. Otherwise returns a data structure
+  that represents the parsed XML."
   [url]
   (println-1 (str "fetching: " url " (" @counter ")"))
   (try
@@ -51,12 +71,26 @@
       (println-1 (str "failed to get " url " " _)))))
 
 (defn maybe-first
+  "If the argument is a non-empty sequence (and not a string),
+  return the first item of that sequence."
   [s]
   (if (string? s) s
     (if (seq s) (first s)
       s)))
 
 (defn parse-blog-raw
+  "Given a tuple of a blog's title, description, RSS feed, and site link,
+  return a tuple that indicates the parsed version of the RSS feed:
+  :ok date-of-last-post title-etc-and-blog-post url-of-blog
+  :gone title-etc url-of-blog
+  :bad title-etc-and-failure-reason url-of-blog
+  We look for the first <item> or <entry>, then look for the first
+  <title> after that to find the first post, and also look for the
+  first likely date (based on heuristic gleaned from looking at
+  lots of RSS and Atom XML data). Date formates seem to be all over
+  the map so we do a fair bit of regex patching to clean things up
+  so that RFC822 and a handful of other 'standard' date parsers
+  actually stand a chance of making sense of the result."
   [[title desc url link]]
   (let [header (if (= title desc) title (str title " - " desc))
         slug (str "<a href=\"" link "\">" header "</a>")]
@@ -98,6 +132,7 @@
       [:gone slug url])))
 
 (defn parse-blog
+  "Parse a blog, counting how many active requests we have in parallel."
   [data]
   (swap! counter inc)
   (let [result (parse-blog-raw data)]
@@ -105,12 +140,18 @@
     result))
 
 (defn parse-opml
+  "Parse the OPML feed list and return a sequence of tuples describing
+  each blog in the list (title, description, RSS feed, blog link)."
   []
   (for [e (xml-seq (get-opml)) :when (= :outline (:tag e))]
     (map (fn [k] (get-in e [:attrs k]))
          [:title :description :xmlUrl :htmlUrl])))
 
 (defn parse-and-sift
+  "For the first N blogs in the OPML feed list, try to parse them
+  all in parallel using futures, then 'join' those threads by
+  forcing a dereference of all the futures, then group them according
+  to success or failure (:ok, :bad, or :gone)."
   [n]
   (->> (vec (take n (parse-opml)))
        (mapv (fn [blog] (future (parse-blog blog))))
@@ -118,6 +159,14 @@
        (group-by first)))
 
 (defn process-blogs
+  "Process the first N blogs on coldfusionbloggers.org and return
+  a summary data structure with counts of how many could not be
+  reached, how many could not be parsed, and how many were parsed
+  successfully, as well as the raw data.
+  Frankly, a map would make more sense here and the calling code
+  could deal with counts, but this started off as just getting
+  the counts, in the REPL and grew into the HTML / JSON producing
+  code that follows..."
   [n]
   (let [blogs (parse-and-sift n)
         gone  (->> blogs :gone (sort-by second) (map rest))
@@ -127,7 +176,9 @@
      gone bad here]))
 
 (defn -main
-  "I don't do a whole lot."
+  "Given a number of blog feeds (which defaults to 30 for testing),
+  analyze that many from coldfusionbloggers.org and produce an HTML
+  file summarizing the results, and a JSON file for Ray Camden."
   [& args]
   (println-1 "Starting to fetch blog data...")
   (let [n-str (or (first args) "30")
